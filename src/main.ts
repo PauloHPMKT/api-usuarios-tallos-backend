3228d4cf5480d2dee10cfbe09420651fc8fef4d4
Paulo Sergio <pauloserg861@gmail.com>
2022-07-06 23:07:49 -0300

    adicionando estrutura de GlobalPipes

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
+/* eslint-disable prettier/prettier */
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes( new ValidationPipe({
+    transform: true,
+  }))
   app.enableCors();
   await app.listen(3001);
 }
