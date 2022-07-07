83fccfb27ef0a5c98b3cb8c6cf7f2e775d6344f7
Paulo Sergio <pauloserg861@gmail.com>
2022-07-06 21:02:44 -0300

    criando collection do database

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { UserSchema } from './schema/user.schema';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'funcionarios', schema: UserSchema }]),
+  ],
   controllers: [UserController],
-  providers: [UserService]
+  providers: [UserService],
 })
 export class UserModule {}
