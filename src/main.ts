4a7bb0c56adf87fd5f31c8d224ea3405769a1386
Paulo Sergio <pauloserg861@gmail.com>
2022-10-06 19:19:02 -0300

    importando dotend

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 /* eslint-disable prettier/prettier */
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
