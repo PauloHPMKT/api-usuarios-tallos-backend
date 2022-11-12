ebd0ed13cd8a5dc8b5b316ca8ea22886e1ea298d
Paulo Sergio <pauloserg861@gmail.com>
2022-11-12 16:02:49 -0300

    atualizando schema do banco de dados

--- src/user/schema/user.schema.ts
+++ src/user/schema/user.schema.ts
@@ -2,9 +2,27 @@
 import * as mongoose from 'mongoose';
 
 export const UserSchema = new mongoose.Schema({
+  cod_user: { type: Number, uniqu: true },
   name: String,
   email: String,
+  contact: String,
+  cpf: String,
+  description: String,
   password: String,
+  location: {
+    address: {
+      street: String,
+      complement: String,
+      zipcode: String,
+      neighborhood: String,
+      city: String,
+      state: String,
+      country: String,
+    }
+  },
   roles: String,
+  dept: String,
+  corporative_email: String,
+  atuation_area: String,
   createdAt: { type: Date, default: Date.now() },
 });
