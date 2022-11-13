fdbc992b603ffb46b80f3ca3fb39c552dd0023b9
Paulo Sergio <pauloserg861@gmail.com>
2022-11-12 21:44:04 -0300

    alterando DTO

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,6 +1,24 @@
 export class CreateUserDto {
+  cod_user: number;
   name: string;
   email: string;
+  contact: string;
+  cpf: string;
+  description: string;
   password: string;
+  location: {
+    address: {
+      street: string;
+      complement: string;
+      zipcode: string;
+      neighborhood: string;
+      city: string;
+      state: string;
+      country: string;
+    };
+  };
   roles: string;
+  dept: string;
+  corporative_email: string;
+  atuation_area: string;
 }
