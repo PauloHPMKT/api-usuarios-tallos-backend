081e3efe45005ca95da5f4e5f5146a37c50fcc38
Paulo Sergio <pauloserg861@gmail.com>
2022-11-13 13:20:39 -0300

    implementando estrategia e guardiao

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { UserModule } from 'src/user/user.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { LocalAuthGuard } from './guards/local-auth.guard';
+import { LocalStrategy } from './strategy/local.strategy';
 
 @Module({
   imports: [UserModule],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, LocalAuthGuard, LocalStrategy],
 })
 export class AuthModule {}
