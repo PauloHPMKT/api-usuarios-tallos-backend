543cae070a646bf0c591aac58507e988743facb7
Paulo Sergio <pauloserg861@gmail.com>
2022-11-13 13:21:15 -0300

    implementando guard

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,21 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  HttpCode,
+  Post,
+  HttpStatus,
+  UseGuards,
+} from '@nestjs/common';
+import { LocalAuthGuard } from './guards/local-auth.guard';
 
 @Controller('login')
 export class AuthController {
 
   @Post()
+  @HttpCode(HttpStatus.OK)
+  @UseGuards(LocalAuthGuard)
   login() {
     console.log('teste de login');
+
+    return;
   }
 }
